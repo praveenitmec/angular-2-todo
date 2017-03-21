e9185f81e9c3cf0a796c28cc7fb939a30d30a419
Praveenkumar <praveenitmec@gmail.com>
2017-03-21 19:28:36 +0530

    Added Dynamic data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,31 @@
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
+    private heroesUrl = 'http://localhost:4000/api/v1/';  // URL to web api
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
-  }
+    constructor(private http: Http) { }
 
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
-  }
+    getHeroes(): Promise<Hero[]> {
+      return this.http.get(this.heroesUrl+'todos')
+                 .toPromise()
+                 .then(response => JSON.parse(response._body) as Hero[])
+                 .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+      console.error('An error occurred', error); // for demo purposes only
+      return Promise.reject(error.message || error);
+    }
+
+    getHero(id: number): Promise<Hero> {
+      return this.http.get(this.heroesUrl+'todos/'+id)
+               .toPromise()
+               .then(response => JSON.parse(response._body) as Hero)
+               .catch(this.handleError);
+    }
 }
