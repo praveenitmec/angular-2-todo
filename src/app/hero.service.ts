c4bac6b8efb3403c0b61145b073e50f1e6282750
Praveenkumar <praveenitmec@gmail.com>
2017-03-22 20:51:00 +0530

    Added CRUD Using Angular2

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,13 +10,40 @@ export class HeroService {
 
     constructor(private http: Http) { }
 
+    private headers = new Headers({ 'Authorization': 'Bearer ' +
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5Ijo'+
+    'iUkY3MEY0eXlxWEV6ZE1na2xxS1FMQXR0IiwiZXhwIjoxNDkwNzE1N'+
+    'jkzLCJpc3MiOiJwcmF2ZWVuX3NlcnZlciIsImF1ZCI6InByYXZlZW5fY2'+
+    'xpZW50In0.p3CmtVQQPoJ9ELB7_6i17qGPOJhcEbjJIrnDUSWBTrU', 'Content-Type': 'application/json'});
+
     private getHeaders(): RequestOptions {
-       let headers =  new Headers({ 'Authorization': 'Bearer ' +
-       'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5Ijo'+
-       'iUkY3MEY0eXlxWEV6ZE1na2xxS1FMQXR0IiwiZXhwIjoxNDkwNzE1N'+
-       'jkzLCJpc3MiOiJwcmF2ZWVuX3NlcnZlciIsImF1ZCI6InByYXZlZW5fY2'+
-       'xpZW50In0.p3CmtVQQPoJ9ELB7_6i17qGPOJhcEbjJIrnDUSWBTrU'});
-       return new RequestOptions({ headers: headers });
+       return new RequestOptions({ headers: this.headers });
+    }
+
+    update(hero: Hero): Promise<Hero> {
+      const url = `${this.heroesUrl}todos/${hero.id}`;
+      return this.http
+        .put(url, JSON.stringify(hero), this.getHeaders())
+        .toPromise()
+        .then(() => hero)
+        .catch(this.handleError);
+    }
+
+    addHero(name: string): Promise<Hero> {
+      const url = `${this.heroesUrl}todos`;
+      return this.http
+        .post(url, JSON.stringify({name: name}), this.getHeaders())
+        .toPromise()
+        .then((response: Response) => response.json() as Hero)
+        .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+      const url = `${this.heroesUrl}todos/${id}`;
+      return this.http.delete(url, this.getHeaders())
+        .toPromise()
+        .then(() => null)
+        .catch(this.handleError);
     }
 
     getHeroes(): Promise<Hero[]> {
